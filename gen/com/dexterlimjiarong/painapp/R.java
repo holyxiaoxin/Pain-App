070f991f0d179ebe2e8511aab80c563392e9b912
JiaRong <jr.loves.maths@gmail.com>
2014-08-28 22:30:38 +0800

    Pain App with basic Custom Navigation template only

--- gen/com/dexterlimjiarong/painapp/R.java
+++ gen/com/dexterlimjiarong/painapp/R.java
@@ -16,9 +16,6 @@ public final class R {
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
     }
-    public static final class array {
-        public static final int navigationDrawerItems=0x7f0c0000;
-    }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
@@ -1080,11 +1077,27 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int homebegin_button=0x7f020057;
-        public static final int ic_drawer=0x7f020058;
-        public static final int ic_launcher=0x7f020059;
-        public static final int normal=0x7f02005a;
-        public static final int pressed=0x7f02005b;
+        public static final int drawer_shadow=0x7f020057;
+        public static final int homebegin_button=0x7f020058;
+        public static final int ic_action_about=0x7f020059;
+        public static final int ic_action_camera=0x7f02005a;
+        public static final int ic_action_cloud=0x7f02005b;
+        public static final int ic_action_email=0x7f02005c;
+        public static final int ic_action_gamepad=0x7f02005d;
+        public static final int ic_action_good=0x7f02005e;
+        public static final int ic_action_group=0x7f02005f;
+        public static final int ic_action_help=0x7f020060;
+        public static final int ic_action_import_export=0x7f020061;
+        public static final int ic_action_labels=0x7f020062;
+        public static final int ic_action_search=0x7f020063;
+        public static final int ic_action_settings=0x7f020064;
+        public static final int ic_action_video=0x7f020065;
+        public static final int ic_drawer=0x7f020066;
+        public static final int ic_launcher=0x7f020067;
+        public static final int normal=0x7f020068;
+        public static final int pressed=0x7f020069;
+        public static final int user1=0x7f02006a;
+        public static final int user2=0x7f02006b;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1098,26 +1111,41 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003f;
+        public static final int action_settings=0x7f05004f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05003e;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
+        public static final int content_frame=0x7f05003d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
+        public static final int drawerSpinner=0x7f050040;
+        public static final int drawerTitle=0x7f050042;
+        public static final int drawer_icon=0x7f050044;
+        public static final int drawer_itemName=0x7f050045;
+        public static final int drawer_layout=0x7f05003c;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int frag1_icon=0x7f050049;
+        public static final int frag1_text=0x7f05004a;
+        public static final int frag2_icon=0x7f05004d;
+        public static final int frag2_text=0x7f05004e;
+        public static final int frag3_icon=0x7f05004b;
+        public static final int frag3_text=0x7f05004c;
+        public static final int headerLayout=0x7f050041;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int itemLayout=0x7f050043;
+        public static final int left_drawer=0x7f05003e;
+        public static final int left_pic=0x7f050046;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1141,11 +1169,12 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
+        public static final int spinnerLayout=0x7f05003f;
         public static final int split_action_bar=0x7f05001e;
+        public static final int sub_text_email=0x7f050048;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003d;
-        public static final int textView2=0x7f05003c;
+        public static final int text_main_name=0x7f050047;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1203,10 +1232,15 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int custom_drawer_item=0x7f030019;
+        public static final int custom_spinner_item=0x7f03001a;
+        public static final int fragment_layout_one=0x7f03001b;
+        public static final int fragment_layout_three=0x7f03001c;
+        public static final int fragment_layout_two=0x7f03001d;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
     }
     public static final class menu {
-        public static final int main=0x7f0d0000;
+        public static final int main=0x7f0c0000;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1251,6 +1285,8 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int drawer_close=0x7f0a0011;
+        public static final int drawer_open=0x7f0a0010;
         public static final int hello_user=0x7f0a000e;
     }
     public static final class style {
--- src/com/dexterlimjiarong/painapp/MainActivity.java
+++ src/com/dexterlimjiarong/painapp/MainActivity.java
@@ -1,177 +1,280 @@
 package com.dexterlimjiarong.painapp;
-
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
-import android.content.Context;
+ 
+import java.util.ArrayList;
+import java.util.List;
+ 
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.res.Configuration;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
-
-public class MainActivity extends ActionBarActivity implements
-	OnItemClickListener {
-	
-	private DrawerLayout drawerLayout;
-	private ListView listView;
-	private String[] socialSites;
-	//private String[] planets;
-	private ActionBarDrawerToggle drawerListener;
-	private MyAdapter myAdapter;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		//planets = getResources().getStringArray(R.array.planets);
-		socialSites = getResources().getStringArray(R.array.navigationDrawerItems);
-		drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-		listView = (ListView) findViewById(R.id.drawerList);
-		myAdapter = new MyAdapter(this);
-		listView.setAdapter(myAdapter);
-
-		// set up the drawer's list view with items and click listener
-//		listView.setAdapter(new ArrayAdapter<>(this,
-//				android.R.layout.simple_list_item_1, planets));
-		listView.setOnItemClickListener(this);
-
-		drawerListener = new ActionBarDrawerToggle(this, drawerLayout,
-				R.drawable.ic_drawer, R.string.drawer_open,
-				R.string.drawer_close) {
-			@Override
-			public void onDrawerClosed(View drawerView) {
-				//super.onDrawerClosed(drawerView);
-				SingleToast.show(MainActivity.this, "Drawer Closed", Toast.LENGTH_SHORT);
-			}
-
-			@Override
-			public void onDrawerOpened(View drawerView) {
-				//super.onDrawerOpened(drawerView);
-				SingleToast.show(MainActivity.this, "Drawer Opened", Toast.LENGTH_SHORT);
-			}
-		};
-		drawerLayout.setDrawerListener(drawerListener);
-		
-		// enable ActionBar app icon to behave as action to toggle nav drawer
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item){
-		// The action bar home/up action should open or close the drawer.
-        // ActionBarDrawerToggle will take care of this.
-		if(drawerListener.onOptionsItemSelected(item)){
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState){
-		super.onPostCreate(savedInstanceState);
-		drawerListener.syncState();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position,
-			long id) {
-		// TODO Auto-generated method stub
-		//SingleToast.show(this, planets[position] + " was selected",
-		//		Toast.LENGTH_SHORT);
-		SingleToast.show(this, socialSites[position] + " was selected",
-				Toast.LENGTH_SHORT);
-		selectItem(position);
-	}
-
-	public void selectItem(int position) {
-		listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-		listView.setItemChecked(position, true);
-		setTitle(socialSites[position]);
-		//setTitle(planets[position]);
-	}
-
-	public void setTitle(String title) {
-		getSupportActionBar().setTitle(title);
-	}
-}
-
-// class to avoid accumulation of toast
-class SingleToast {
-	private static Toast mToast;
-
-	public static void show(Context context, String text, int duration) {
-		if (mToast != null)
-			mToast.cancel();
-		mToast = Toast.makeText(context, text, duration);
-		mToast.show();
-	}
-
-}
-
-class MyAdapter extends BaseAdapter{
-	private Context context;
-	String[] socialSites;
-	int[] images = {R.drawable.ic_facebook, R.drawable.ic_facebook, 
-			R.drawable.ic_facebook, R.drawable.ic_facebook, R.drawable.ic_facebook};
-	
-	public MyAdapter(Context context){
-		this.context=context;
-		socialSites=context.getResources().getStringArray(R.array.social);
-	}
-	@Override
-	public int getCount() {
-		// TODO Auto-generated method stub
-		return socialSites.length;
-	}
-
-	@Override
-	public Object getItem(int position) {
-		// TODO Auto-generated method stub
-		return socialSites[position];
-	}
-
-	@Override
-	public long getItemId(int position) {
-		// TODO Auto-generated method stub
-		return position;
-	}
-
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		View row = null;
-		// TODO Auto-generated method stub
-		if(convertView==null){
-			LayoutInflater inflater = (LayoutInflater) context
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			row = inflater.inflate(R.layout.custom_row, parent, false);
-		}else{
-			row = convertView;
-		}
-		TextView titleTextView = (TextView) row.findViewById(R.id.textView1);
-		ImageView titleImageView = (ImageView) row.findViewById(R.id.imageView1);
-		titleTextView.setText(socialSites[position]);
-		titleImageView.setImageResource(images[position]);
-		
-		return row;
-	}
-	
+ 
+public class MainActivity extends Activity {
+ 
+      private DrawerLayout mDrawerLayout;
+      private ListView mDrawerList;
+      private ActionBarDrawerToggle mDrawerToggle;
+ 
+      private CharSequence mDrawerTitle;
+      private CharSequence mTitle;
+      CustomDrawerAdapter adapter;
+ 
+      List<DrawerItem> dataList;
+ 
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+ 
+            // Initializing
+            dataList = new ArrayList<DrawerItem>();
+            mTitle = mDrawerTitle = getTitle();
+            mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+            mDrawerList = (ListView) findViewById(R.id.left_drawer);
+ 
+            mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
+                        GravityCompat.START);
+ 
+         // Add Drawer Item to dataList
+            dataList.add(new DrawerItem(true)); // adding a spinner to the list
+ 
+            dataList.add(new DrawerItem("My Favorites")); // adding a header to the list
+            dataList.add(new DrawerItem("Message", R.drawable.ic_action_email));
+            dataList.add(new DrawerItem("Likes", R.drawable.ic_action_good));
+            dataList.add(new DrawerItem("Games", R.drawable.ic_action_gamepad));
+            dataList.add(new DrawerItem("Lables", R.drawable.ic_action_labels));
+ 
+             dataList.add(new DrawerItem("Main Options"));// adding a header to the list
+            dataList.add(new DrawerItem("Search", R.drawable.ic_action_search));
+            dataList.add(new DrawerItem("Cloud", R.drawable.ic_action_cloud));
+            dataList.add(new DrawerItem("Camara", R.drawable.ic_action_camera));
+            dataList.add(new DrawerItem("Video", R.drawable.ic_action_video));
+            dataList.add(new DrawerItem("Groups", R.drawable.ic_action_group));
+            dataList.add(new DrawerItem("Import & Export",
+                        R.drawable.ic_action_import_export));
+ 
+             dataList.add(new DrawerItem("Other Option")); // adding a header to the list
+            dataList.add(new DrawerItem("About", R.drawable.ic_action_about));
+            dataList.add(new DrawerItem("Settings", R.drawable.ic_action_settings));
+            dataList.add(new DrawerItem("Help", R.drawable.ic_action_help));
+ 
+            adapter = new CustomDrawerAdapter(this, R.layout.custom_drawer_item,
+                        dataList);
+ 
+            mDrawerList.setAdapter(adapter);
+ 
+            mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+ 
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+            getActionBar().setHomeButtonEnabled(true);
+ 
+            mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                        R.drawable.ic_drawer, R.string.drawer_open,
+                        R.string.drawer_close) {
+                  public void onDrawerClosed(View view) {
+                        getActionBar().setTitle(mTitle);
+                        invalidateOptionsMenu(); // creates call to
+                                                                  // onPrepareOptionsMenu()
+                  }
+ 
+                  public void onDrawerOpened(View drawerView) {
+                        getActionBar().setTitle(mDrawerTitle);
+                        invalidateOptionsMenu(); // creates call to
+                                                                  // onPrepareOptionsMenu()
+                  }
+            };
+ 
+            mDrawerLayout.setDrawerListener(mDrawerToggle);
+ 
+            if (savedInstanceState == null) {
+            	 
+                if (dataList.get(0).isSpinner()
+                            & dataList.get(1).getTitle() != null) {
+                      SelectItem(2);
+                } else if (dataList.get(0).getTitle() != null) {
+                      SelectItem(1);
+                } else {
+                      SelectItem(0);
+                }
+          }
+ 
+      }
+ 
+      @Override
+      public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+      }
+ 
+      
+      //You need to update this according to the “dataList” item position. 
+      //Fragment should be only need to change when we click in a selectable 
+      //item not in header or spinner.  So change the case statements to point 
+      //correct selectable item. (Be careful you may get runtime errors if you 
+      //update incorrectly.) I delete the  0,1 and 6 case statement and 
+      //add new case statement for 14,15 and 16 positions .
+      public void SelectItem(int possition) {
+    	  
+          Fragment fragment = null;
+          Bundle args = new Bundle();
+          switch (possition) {
+     
+          case 2:
+                fragment = new FragmentThree();
+                args.putString(FragmentThree.ITEM_NAME, dataList.get(possition)
+                            .getItemName());
+                args.putInt(FragmentThree.IMAGE_RESOURCE_ID, dataList
+                            .get(possition).getImgResID());
+                break;
+          case 3:
+                fragment = new FragmentOne();
+                args.putString(FragmentOne.ITEM_NAME, dataList.get(possition)
+                            .getItemName());
+                args.putInt(FragmentOne.IMAGE_RESOURCE_ID, dataList.get(possition)
+                            .getImgResID());
+                break;
+          case 4:
+                fragment = new FragmentTwo();
+                args.putString(FragmentTwo.ITEM_NAME, dataList.get(possition)
+                            .getItemName());
+                args.putInt(FragmentTwo.IMAGE_RESOURCE_ID, dataList.get(possition)
+                            .getImgResID());
+                break;
+          case 5:
+                fragment = new FragmentThree();
+                args.putString(FragmentThree.ITEM_NAME, dataList.get(possition)
+                            .getItemName());
+                args.putInt(FragmentThree.IMAGE_RESOURCE_ID, dataList
+                            .get(possition).getImgResID());
+                break;
+          case 7:
+                fragment = new FragmentTwo();
+                args.putString(FragmentTwo.ITEM_NAME, dataList.get(possition)
+                            .getItemName());
+                args.putInt(FragmentTwo.IMAGE_RESOURCE_ID, dataList.get(possition)
+                            .getImgResID());
+                break;
+          case 8:
+                fragment = new FragmentThree();
+                args.putString(FragmentThree.ITEM_NAME, dataList.get(possition)
+                            .getItemName());
+                args.putInt(FragmentThree.IMAGE_RESOURCE_ID, dataList
+                            .get(possition).getImgResID());
+                break;
+          case 9:
+                fragment = new FragmentOne();
+                args.putString(FragmentOne.ITEM_NAME, dataList.get(possition)
+                            .getItemName());
+                args.putInt(FragmentOne.IMAGE_RESOURCE_ID, dataList.get(possition)
+                            .getImgResID());
+                break;
+          case 10:
+                fragment = new FragmentTwo();
+                args.putString(FragmentTwo.ITEM_NAME, dataList.get(possition)
+                            .getItemName());
+                args.putInt(FragmentTwo.IMAGE_RESOURCE_ID, dataList.get(possition)
+                            .getImgResID());
+                break;
+          case 11:
+                fragment = new FragmentThree();
+                args.putString(FragmentThree.ITEM_NAME, dataList.get(possition)
+                            .getItemName());
+                args.putInt(FragmentThree.IMAGE_RESOURCE_ID, dataList
+                            .get(possition).getImgResID());
+                break;
+          case 12:
+                fragment = new FragmentOne();
+                args.putString(FragmentOne.ITEM_NAME, dataList.get(possition)
+                            .getItemName());
+                args.putInt(FragmentOne.IMAGE_RESOURCE_ID, dataList.get(possition)
+                            .getImgResID());
+                break;
+          case 14:
+                fragment = new FragmentThree();
+                args.putString(FragmentThree.ITEM_NAME, dataList.get(possition)
+                            .getItemName());
+                args.putInt(FragmentThree.IMAGE_RESOURCE_ID, dataList.get(possition)
+                            .getImgResID());
+                break;
+          case 15:
+                fragment = new FragmentOne();
+                args.putString(FragmentOne.ITEM_NAME, dataList.get(possition)
+                            .getItemName());
+                args.putInt(FragmentOne.IMAGE_RESOURCE_ID, dataList.get(possition)
+                            .getImgResID());
+                break;
+          case 16:
+                fragment = new FragmentTwo();
+                args.putString(FragmentTwo.ITEM_NAME, dataList.get(possition)
+                            .getItemName());
+                args.putInt(FragmentTwo.IMAGE_RESOURCE_ID, dataList.get(possition)
+                            .getImgResID());
+                break;
+          default:
+                break;
+          }
+     
+          fragment.setArguments(args);
+          FragmentManager frgManager = getFragmentManager();
+          frgManager.beginTransaction().replace(R.id.content_frame, fragment)
+                      .commit();
+     
+          mDrawerList.setItemChecked(possition, true);
+          setTitle(dataList.get(possition).getItemName());
+          mDrawerLayout.closeDrawer(mDrawerList);
+     
+    }
+ 
+      @Override
+      public void setTitle(CharSequence title) {
+            mTitle = title;
+            getActionBar().setTitle(mTitle);
+      }
+ 
+      @Override
+      protected void onPostCreate(Bundle savedInstanceState) {
+            super.onPostCreate(savedInstanceState);
+            // Sync the toggle state after onRestoreInstanceState has occurred.
+            mDrawerToggle.syncState();
+      }
+ 
+      @Override
+      public void onConfigurationChanged(Configuration newConfig) {
+            super.onConfigurationChanged(newConfig);
+            // Pass any configuration change to the drawer toggles
+            mDrawerToggle.onConfigurationChanged(newConfig);
+      }
+ 
+      @Override
+      public boolean onOptionsItemSelected(MenuItem item) {
+            // The action bar home/up action should open or close the drawer.
+            // ActionBarDrawerToggle will take care of this.
+            if (mDrawerToggle.onOptionsItemSelected(item)) {
+                  return true;
+            }
+ 
+            return false;
+      }
+ 
+        private class DrawerItemClickListener implements
+                  ListView.OnItemClickListener {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                        long id) {
+            	if (dataList.get(position).getTitle() == null) {
+                    SelectItem(position);
+              }
+ 
+            }
+      }
+ 
 }
